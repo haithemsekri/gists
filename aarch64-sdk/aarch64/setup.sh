1b9a11ea7bc89e23b6e7baa9697ea2af5c0f2767
haithem.sekri <haithemsekri@gmail.com>
2022-05-26 19:27:39 +0200

    changes

--- aarch64-sdk/aarch64/setup.sh
+++ aarch64-sdk/aarch64/setup.sh
@@ -4,7 +4,7 @@ CURR_DIR="$(dirname $(realpath $0))"
 
 SetupToolchain() {
   rm -rf $CURR_DIR/gcc-aarch64-linux
-  [[ ! -f $CURR_DIR/gcc-aarch64-linux.tar.xz ]] && wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/11.2-2022.02/binrel/gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu.tar.xz -O $CURR_DIR/gcc-aarch64-linux.tar.xz
+  [[ ! -f $CURR_DIR/gcc-aarch64-linux.tar.xz ]] && wget https://github.com/haithemsekri/gists/releases/download/v0.1/gcc-arm-11.2-2022.02-x86_64-aarch64-none-linux-gnu.tar.xz -O $CURR_DIR/gcc-aarch64-linux.tar.xz
   mkdir $CURR_DIR/gcc-aarch64-linux.tmp
   tar -xf $CURR_DIR/gcc-aarch64-linux.tar.xz -C $CURR_DIR/gcc-aarch64-linux.tmp/
   dir_ls="$CURR_DIR/gcc-aarch64-linux.tmp/$(ls $CURR_DIR/gcc-aarch64-linux.tmp/)"
